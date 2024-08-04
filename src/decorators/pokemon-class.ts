41e5bf9a6520309fb0e3db4426a49311be5707a4
LillaDou <lilla.e.j.du@gmail.com>
2024-08-04 14:56:07 +0200

    Decoradores de métodos

--- src/decorators/pokemon-class.ts
+++ src/decorators/pokemon-class.ts
@@ -34,11 +34,35 @@ const bloquearPrototipo = function( constructor: Function) {
 //cambios al constructor y al prototipo, o en general, que se sobreescriban datos en nuestras clases 
 
 
+//*Factory decorator para un método:
+//Target: dependerá de lo que coloquemos
+//PropertyKey: nombre de nuestro método para el cual pondremos el decorador
+//Descriptor: puede servir para hacer el read only
+function CheckValidPokemonId() {
+    return function( target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        
+        const originalMethod = descriptor.value;
+
+        //El argumento del descriptor.value va a ser el mismo argumento de la función
+        //a la que queremos añadir el decorador
+        descriptor.value = ( id: number ) => {
+            if( id < 1 || id > 800 ) {
+                return console.error('El id del pokemon debe de estar entre 1 y 800');
+            } else {
+                originalMethod( id );
+            }
+        }
+        // descriptor.value = () => console.log('Hola Mundo');
+
+    }
+}
+
+
 
 //Se pueden anidar decoradores. Se van a ejecutar en orden secuencial
 //@printToConsole
 @bloquearPrototipo
-@printToConsoleConditional( true ) //true o false en base a si queremos imprimirlo en consola
+@printToConsoleConditional( false ) //true o false en base a si queremos imprimirlo en consola
 export class Pokemon {
 
     public publicApi: string = 'https://pokeapi.co';
@@ -46,5 +70,14 @@ export class Pokemon {
     constructor(
         public name: string,
     ){}
+
+    @CheckValidPokemonId()
+    savePokemonToDB( id: number) {
+        console.log(`Pokemon guardado en DB ${ id }`)
+    }
+    //Con los decoradores de métodos, podemos hacer validaciones. De esta manera no
+    //tenemos que copiar código múltiples veces en métodos, sino que ya irá incorporado
+    //el método con el decorador 
+    //Ponemos los paréntesis al decorador pues es un factory decorator
 }
 
